5ead557f8ebe2d306d3cb96b2f2218d67e31d831
Sebastian Baez <sebaez200011@gmail.com>
2023-01-11 11:14:18 -0500

    Payment and Delivery models

---
+++ carboneros/payments/apps.py
@@ -0,0 +1,7 @@
+# Django
+from django.apps import AppConfig
+
+
+class PaymentsAppConfig(AppConfig):
+    name = "carboneros.payments"
+    verbose_name = "Payments"
---
+++ carboneros/payments/models/payments.py
@@ -0,0 +1,12 @@
+# Django
+from django.db import models
+
+
+class Payment(models.Model):
+
+    amount = models.IntegerField()
+    customer_email = models.EmailField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.customer_email} - {self.amount}'
