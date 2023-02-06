7820ab2ab93708e9ea11d9884deb91c8c7edfa24
MaxVGC <carlos.andres12390@live.com>
2023-02-05 22:51:30 -0500

    fix categories

--- src/pages/Store/Store.jsx
+++ src/pages/Store/Store.jsx
@@ -12,10 +12,21 @@ import DrinksImg from '../../Assets/images/Store/Categories/drinks.jpg'
 import Loader from '../../components/Loader'
 import FoodCard from '../../components/FoodCard'
 import { useDispatch, useSelector } from 'react-redux'
-import FoodDetails from '../../components/FoodDetails'
 export default function Store({ navBar }) {
     const shopStore = useSelector(store => store.shop);
     const dispatch = useDispatch();
+    const [categories, setCategories] = useState({
+        current: null
+    });
+    const [search, setSearch] = useState(null);
+
+    function changeCategorie(cat) {
+        if (categories.current == cat) {
+            setCategories({ current: null })
+        } else {
+            setCategories({ current: cat })
+        }
+    }
 
     const executeDispatch = (type, target) => {
         dispatch({
@@ -39,7 +50,7 @@ export default function Store({ navBar }) {
             executeDispatch("UPDATE_SHOP_DATA", response.comidas);
         });
     }, [])
-    
+
     return (
         <div id="store_page" className='row'>
             <div className="col-md-3 options-sidebar">
@@ -49,52 +60,52 @@ export default function Store({ navBar }) {
                 </span>
                 <div className="categories">
                     <div className="list-food-card">
-                        <div className="food-card" style={{ backgroundImage: 'url(' + CreamSoupImg + ')' }}>
+                        <div className="food-card" style={{ backgroundImage: 'url(' + CreamSoupImg + ')' }} onClick={() => (changeCategorie("Cremas"))}>
                             <div className="bg">
                                 <span>Cremas</span>
                             </div>
                         </div>
-                        <div className="food-card" style={{ backgroundImage: 'url(' + ChorizoImg + ')' }}>
+                        <div className="food-card" style={{ backgroundImage: 'url(' + ChorizoImg + ')' }} onClick={() => (changeCategorie("Entradas"))}>
                             <div className="bg">
                                 <span>Entradas</span>
                             </div>
                         </div>
-                        <div className="food-card" style={{ backgroundImage: 'url(' + PigImg + ')' }}>
+                        <div className="food-card" style={{ backgroundImage: 'url(' + PigImg + ')' }} onClick={() => (changeCategorie("Cerdo"))}>
                             <div className="bg">
                                 <span>Cerdo</span>
                             </div>
                         </div>
-                        <div className="food-card" style={{ backgroundImage: 'url(' + ChickenImg + ')' }}>
+                        <div className="food-card" style={{ backgroundImage: 'url(' + ChickenImg + ')' }} onClick={() => (changeCategorie("Pollo"))}>
                             <div className="bg">
                                 <span>Pollo</span>
                             </div>
                         </div>
-                        <div className="food-card" style={{ backgroundImage: 'url(' + FishImg + ')' }}>
+                        <div className="food-card" style={{ backgroundImage: 'url(' + FishImg + ')' }} onClick={() => (changeCategorie("Pescado"))}>
                             <div className="bg">
                                 <span>Pescado</span>
                             </div>
                         </div>
-                        <div className="food-card" style={{ backgroundImage: 'url(' + BeefImg + ')' }}>
+                        <div className="food-card" style={{ backgroundImage: 'url(' + BeefImg + ')' }} onClick={() => (changeCategorie("Carne de res"))}>
                             <div className="bg">
                                 <span>Carne de res</span>
                             </div>
                         </div>
-                        <div className="food-card" style={{ backgroundImage: 'url(' + SpecialsImg + ')' }}>
+                        <div className="food-card" style={{ backgroundImage: 'url(' + SpecialsImg + ')' }} onClick={() => (changeCategorie("Especiales"))}>
                             <div className="bg">
                                 <span>Especiales</span>
                             </div>
                         </div>
-                        <div className="food-card" style={{ backgroundImage: 'url(' + OthersImg + ')' }}>
+                        <div className="food-card" style={{ backgroundImage: 'url(' + OthersImg + ')' }} onClick={() => (changeCategorie("Otros"))}>
                             <div className="bg">
                                 <span>Otros</span>
                             </div>
                         </div>
-                        <div className="food-card" style={{ backgroundImage: 'url(' + AditionalsImg + ')' }}>
+                        <div className="food-card" style={{ backgroundImage: 'url(' + AditionalsImg + ')' }} onClick={() => (changeCategorie("Adicionales"))}>
                             <div className="bg">
                                 <span>Adicionales</span>
                             </div>
                         </div>
-                        <div className="food-card" style={{ backgroundImage: 'url(' + DrinksImg + ')' }}>
+                        <div className="food-card" style={{ backgroundImage: 'url(' + DrinksImg + ')' }} onClick={() => (changeCategorie("Bebidas"))}>
                             <div className="bg">
                                 <span>Bebidas</span>
                             </div>
@@ -105,7 +116,7 @@ export default function Store({ navBar }) {
             <div className="col-md-9 content">
                 <div className="search-bar row">
                     <div className="search-input">
-                        <input type="text" name="" id="" placeholder='¿Qué te apetece hoy?' />
+                        <input type="text" name="" id="" placeholder='¿Qué te apetece hoy?' onChange={(e) => (setSearch(e.target.value))} />
                         <ion-icon name="search"></ion-icon>
                     </div>
                 </div>
@@ -116,7 +127,23 @@ export default function Store({ navBar }) {
                         ) : (
                             shopStore.dataFood.map((e, i) => (
                                 <>
-                                    <FoodCard data={e} key={i} n={i} />
+                                    {categories.current == null ? (
+                                        search==null || e.nombre.toLowerCase().includes(search.toLowerCase())?(
+                                            <FoodCard data={e} key={i} n={i} />
+                                        ):(
+                                            null
+                                        )
+                                    ) : (
+                                        e.categorias.includes(categories.current) ? (
+                                            search==null || e.nombre.toLowerCase().includes(search.toLowerCase())?(
+                                                <FoodCard data={e} key={i} n={i} />
+                                            ):(
+                                                null
+                                            )
+                                        ) : (
+                                            null
+                                        )
+                                    )}
                                 </>
                             ))
                         )
